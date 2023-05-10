caf36e1e0a21b8470b6c8790540dd96794865431
Augusto Medeiros <augustomedeiros451@gmail.com>
2023-05-09 22:15:44 -0300

    create form food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,33 @@
 class FoodsController < ApplicationController
+  before_action :set_food, only: [:show]
+
   def index
     @foods = Food.all
+  end
+
+  def show; end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user_id = 3
+    if @food.save
+      redirect_to food_path(@food)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :category, :price, :description, :image_url)
+  end
+
+  def set_food
+    @food = Food.find(params[:id])
   end
 end
